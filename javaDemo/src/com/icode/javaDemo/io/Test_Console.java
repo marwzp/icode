014864815466bbf42bfa7b498a72183c4d70bb47
marswzp <wangzepengmr@sina.com>
2017-12-01 13:44:25 +0800

    测试Console类

--- javaDemo/src/com/icode/javaDemo/io/Test_Console.java
+++ javaDemo/src/com/icode/javaDemo/io/Test_Console.java
@@ -1,11 +1,23 @@
 
 package com.icode.javaDemo.io;
 
+import java.io.Console;
 
+/**
+ * Console类测试
+ * TODO javadoc for com.icode.javaDemo.io.Test_Console
+ * @Copyright: 2017海南易建科技股份有限公司
+ * @author: wang-zepeng
+ * @since: 2017年12月1日
+ */
 public class Test_Console {
 
 	public static void main(String[] args) {
-		
+		Console csl = System.console();
+		String userName = csl.readLine("User name:");
+		csl.printf("user:"+userName+"\n");
+		char[] password = csl.readPassword("password:");
+		csl.printf(String.valueOf(password));
 	}
 	
 	
